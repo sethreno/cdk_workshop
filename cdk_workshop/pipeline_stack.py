1b687bf9d2739c57b2421bf8d7785ef587a0b28f
Seth Reno <sethreno@gmail.com>
2025-02-08 07:05:03 -0600

    Connect pipleine to github

--- cdk_workshop/pipeline_stack.py
+++ cdk_workshop/pipeline_stack.py
@@ -1,14 +1,24 @@
+import aws_cdk as cdk
 from constructs import Construct
-from aws_cdk import (
-    Stack,
-    aws_codepipeline as codepipeline,
-)
+from aws_cdk.pipelines import CodePipeline, CodePipelineSource, ShellStep
 
 
-class WorkshopPipelineStack(Stack):
-    def __init__(self, scope: Construct, id: str, **kwargs) -> None:
-        super().__init__(scope, id, **kwargs)
+class WorkshopPipelineStack(cdk.Stack):
 
-        repo = codepipeline.Repository(
-            self, "WorkshopRepo", repository_name="WorkshopRepo"
+    def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
+        super().__init__(scope, construct_id, **kwargs)
+
+        pipeline = CodePipeline(
+            self,
+            "Pipeline",
+            pipeline_name="MyPipeline",
+            synth=ShellStep(
+                "Synth",
+                input=CodePipelineSource.git_hub("sethreno/cdk_workshop", "main"),
+                commands=[
+                    "npm install -g aws-cdk",
+                    "python -m pip install -r requirements.txt",
+                    "cdk synth",
+                ],
+            ),
         )
